c41bf5866d5a32a1445b7e3842f73bf9c4aedc14
Evan Graïne <evan.graine@gmail.com>
2022-06-30 18:30:50 +0200

    Comments

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,7 +7,7 @@ import DeroBridgeApi from 'dero-rpc-bridge-api'
 import React from 'react';
 import MyComponent from './components/MyComponent';
 
-
+// Enum to track the connection status of the bridge
 export enum ConnectionStatus {
   NotConnected = "not connected",
   Connected = "connected to the bridge!",
@@ -15,36 +15,54 @@ export enum ConnectionStatus {
   Failed = 'connection failed !'
 }
 
+// Global context variable to use all across the app
 export const DeroContext = React.createContext<DeroBridgeApi>(null);
 
+// App component
 function App() {
+  // The bridge object given to the DeroContext
   const [bridge, setBridge] = useState<DeroBridgeApi>()
+
+  // The connection status : not connected by default
   const [status, setStatus] = useState<ConnectionStatus>(ConnectionStatus.NotConnected)
 
+  // Callback when we click the connect button
   const connect = useCallback(async () => {
+    // When clicked set status to connecting
     setStatus(ConnectionStatus.Connecting);
 
+    // Instanciate the bridge
     const deroBridgeApi = new DeroBridgeApi();
 
+    // Call bridge init function
     deroBridgeApi.init()
-      .then(() => {
+      .then(() => { // When successful
+        // Update the connection status
         setStatus(ConnectionStatus.Connected);
       })
-      .catch((err: any) => {
+      .catch((err: any) => { // When we catch an error
+        // Update the connection status with the error message
         setStatus(err.message);
       })
 
+    // We keep the bridge object in the app's state, so that other components can interact with it
     setBridge(deroBridgeApi);
   }, [])
 
   return (
+    // Pass the bridge object to the context provider
+    // The app has a title a button calling the connect callback defined earlier and a custom component defined in components/MyComponent.tsx
     <DeroContext.Provider value={bridge}>
       <div className='App'>
-      <h1>Dero RPC Bridge API example app</h1>
+        <h1>Dero RPC Bridge API example app</h1>
+        
         <button onClick={connect}>connect</button>
+        
         <div>Status:</div>
         <div><i>{status}</i></div>
+        
         <MyComponent status={status}/>
+
       </div>
     </DeroContext.Provider>
   )
--- example/src/components/MyComponent.tsx
+++ example/src/components/MyComponent.tsx
@@ -1,34 +1,45 @@
 import { useCallback, useContext, useState } from "react"
 import { ConnectionStatus, DeroContext } from "../App";
 
+// Define the props of our component
 interface Props {
     status: ConnectionStatus
 }
 
+// MyComponent definition
 export default (props: Props) => {
+    // Balance and error message, updated when clicking the Get Balance button
     const [balance, setBalance] = useState<string | number>('unknown');
     const [errorMessage, setErrorMessage] = useState<string>();
 
+    // Here we get the bridge object from the context
     const deroBridgeApi = useContext(DeroContext);
     
+    // Callback when clicking the Get Balance button
     const getBalance = useCallback(async () => {
-        const api = deroBridgeApi;
-        api.wallet('get-balance')
-            .then((response: any) => {
-                console.log(response)
+        // Call the GetBalance method from the wallet
+        deroBridgeApi.wallet('get-balance')
+            .then((response: any) => { // When successful
+                // Update the balance state of the component
                 setBalance(response.data.result.balance / 100000)
+                // Empty the error message
                 setErrorMessage("");
             })
-            .catch((err: any) => {
-                console.log(err);
-                setErrorMessage("Failed");
+            .catch((err: any) => { // When failed
+                // Update the balance to unknown
+                setBalance('unknown');
+                // Update the error message
+                setErrorMessage(err.message);
             })
-    }, [props.status])
+    }, [props.status]) // Update the function when the connection status change so that the context actually provide a deroBridgeApi object
 
+    // We render the component only if status is connected 
     if (props.status == ConnectionStatus.Connected) {
         return <div className='MyComponent'>
             <h2>Inner compoent</h2>
+
             <button onClick={getBalance}>Get Wallet Balance</button>
+            
             <div>Balance:</div>
             <div>{balance}</div>
             <div>{errorMessage}</div>
